dee0f25ec42a023e9b59c511130553a09c6a6c59
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2023-08-02 02:34:05 +0530

    Fixed some error

--- src/components/WeeklyWorkingDays.jsx
+++ src/components/WeeklyWorkingDays.jsx
@@ -40,20 +40,21 @@ export default function WeeklyWorkingDays({
         workingDays.map((day, index) => {
           const date = moment(selectedDate).day(index + 1);
 
-          // Find the dataItem corresponding to the current date
           const dataItem = checkedDetails.find(
             (item) => item.Date === date.format("YYYY-MM-DD")
           );
 
           return (
             <div key={index} className="flex gap-2 my-2">
-              {/* ... (unchanged) */}
+              <div className=" w-24 ">
+                <div className="">{day}</div>
+                <div className="">{date.format("MM/DD")}</div>
+              </div>
               <div className=" w-4/5 ">
                 {workingHours &&
                   workingHours.map((hour, index) => {
                     const time = hour.time;
 
-                    // Check if the checkbox should be checked based on dataItem's Time
                     const isChecked = dataItem && dataItem.Time === time;
 
                     return (
@@ -64,11 +65,6 @@ export default function WeeklyWorkingDays({
                           id={index}
                           name={hour.time}
                           value={hour.time}
-                          onChange={() => {
-                            // You can implement your checkbox change handling here
-                            // This function will be called when the checkbox is clicked
-                            // You can update the `checkedDetails` state based on the checkbox changes
-                          }}
                         />
                         <label htmlFor="" className="pl-1">
                           {hour.time}
